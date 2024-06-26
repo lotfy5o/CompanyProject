9916b226c1e155df31a3062952896eede0745c0f
lotfy5o <lotfymoh1235o@gmail.com>
2024-06-26 13:44:36 +0200

    email column in Message Module is unique

--- database/migrations/2024_06_26_093948_create_messages_table.php
+++ database/migrations/2024_06_26_093948_create_messages_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('messages', function (Blueprint $table) {
             $table->increments('id');
             $table->string('name')->nullable();
-            $table->string('email')->nullable();
+            $table->string('email')->nullable()->unique();
             $table->string('subject')->nullable();
             $table->text('message')->nullable();
             $table->timestamps();
